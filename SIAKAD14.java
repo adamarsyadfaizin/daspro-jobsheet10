5a74a50eff2a21576ce0898441a2f4096f72479c
adamarsyadfaizin <adamkian09@gmail.com>
2024-11-06 17:17:50 +0700

    CommitPercobaan4Pertanyaan

---
+++ SIAKAD14.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SIAKAD14 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jmlMhs, matkul;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jmlMhs = sc.nextInt();
+        System.out.print("Masukkan jumlah matkul: ");
+        matkul = sc.nextInt();
+        int[][] nilai = new int[jmlMhs][matkul];
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("input nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai [i][j];
+            }
+
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/matkul);
+        }
+        System.out.println("\n====================================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+
+        for (int j = 0; j < matkul; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < jmlMhs; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jmlMhs);
+        }
+    }
+}
